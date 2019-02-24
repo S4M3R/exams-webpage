320efe9992ffea469d0e6cfe9fd8062bc5a7715b
Agustin Suarez <samerstudio@outlook.com>
2019-02-24 20:20:42 -0300

    Now querrys send err in the callback

--- db/user.js
+++ db/user.js
@@ -3,15 +3,13 @@ var con = require('./connect.js')
 
 var getUserByID = (id, callback) =>{
   con.query(`SELECT * FROM User WHERE ID = ${id};`, function (err, selectResult) {
-    if (err) throw err;
-    callback(selectResult)
+    callback(selectResult, err)
   });
 }
 
 var getUsersByCountry = (country, callback) =>{
   con.query(`SELECT * FROM User WHERE Country = ${country};`, function (err, selectResult) {
-    if (err) throw err;
-    callback(selectResult)
+    callback(selectResult, err)
   });
 }
 
