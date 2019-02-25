2676d335fcc4fe31ac135a7af86e559eedd816b9
Agustin Suarez <samerstudio@outlook.com>
2019-02-25 01:43:36 -0300

    Querry Joins with ranks and added getRanks

--- db/user.js
+++ db/user.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var con = require('./connect.js')
 
 var getUserByID = (id, callback) =>{
-  con.query(`SELECT * FROM User WHERE ID = ${id};`, function (err, selectResult) {
+  con.query(`SELECT * FROM user u JOIN rank r ON u.rank = r.name WHERE ID = ${id};`, function (err, selectResult) {
     callback(selectResult, err)
   });
 }
@@ -13,6 +13,13 @@ var getUsersByCountry = (country, callback) =>{
   });
 }
 
+var getRanks = (callback) => {
+  con.query(`SELECT * FROM rank;`, function (err,selectResult) {
+    callback(selectResult,err)
+  })
+}
+
 module.exports = {
-  getUserByID
+  getUserByID,
+  getRanks
 }
