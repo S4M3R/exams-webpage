c8e0bd9a437f12a9a280a8459d3852549ca4644f
Agustin Suarez <samerstudio@outlook.com>
2019-02-25 01:44:53 -0300

    Added api/ranks and send err to client

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,19 @@ const USERS = require('../db/user.js')
 
 router.get('/user/:id',(req,res) => {
   USERS.getUserByID(parseInt(req.params.id), (user, err) => {
+    if(err){
+      res.send(err)
+    }
     res.send(user)
+  })
+})
+
+router.get('/ranks',(req,res) => {
+  USERS.getRanks((ranks, err) => {
+    if(err){
+      res.send(err)
+    }
+    res.send(ranks)
   })
 })
 
